751829b55911116cf32cd3e89090e1d2048bbe62
likeCoder0 <2021.anchal.sharma@ves.ac.in>
2025-03-31 14:13:37 +0530

    first commit

---
+++ server/app/api/chat/route.ts
@@ -0,0 +1,47 @@
+import { StreamingTextResponse } from 'ai'
+// import { experimental_buildLlama2Prompt } from 'ai/prompts'
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge'
+
+export async function POST(req: Request) {
+  // Extract the `messages` from the body of the request
+  const { messages } = await req.json();
+
+  // Create a text generation stream
+  const result = await model.generateContent({
+    contents: [
+      {
+        role: 'user',
+        parts: [
+          {
+            text: messages,
+          },
+        ],
+      },
+    ],
+    generationConfig: {
+      maxOutputTokens: 500,
+      temperature: 0.1,
+    },
+  });
+
+  // Call text() method and clean the response
+  const rawText = await result.response.text(); // Call the text() method
+  const cleanedText = rawText.replace(/\*/g, ''); // Remove all asterisks (*)
+
+  // Respond with the cleaned stream
+  return new StreamingTextResponse(cleanedText, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST',
+    },
+  });
+}
